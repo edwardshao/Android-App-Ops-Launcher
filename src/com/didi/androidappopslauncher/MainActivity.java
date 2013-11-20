a513a06c719bc6870fa4d18258beb0b025530514
Edward Shao <laface.tw@gmail.com>
2013-11-21 00:19:12 +0800

    Handle ActivityNotFoundException exception

--- src/com/didi/androidappopslauncher/MainActivity.java
+++ src/com/didi/androidappopslauncher/MainActivity.java
@@ -1,7 +1,9 @@
 package com.didi.androidappopslauncher;
 
 import android.os.Bundle;
+import android.widget.Toast;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 
 public class MainActivity extends Activity {
@@ -9,12 +11,22 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		boolean bActivityNotFound = false;
 		Intent intent = new Intent();
-		intent.setAction("android.settings.APP_OPS_SETTINGS");
-
-		startActivity(intent);
 		
-		finish();
+		try {
+			intent.setAction("android.settings.APP_OPS_SETTINGS");
+			startActivity(intent);
+		} catch (ActivityNotFoundException e) {
+			bActivityNotFound = true;
+		} 
+		
+		finally {
+			if (bActivityNotFound) {
+				Toast.makeText(getApplicationContext(), "Ops Activity is Not Found!!", 
+								Toast.LENGTH_SHORT).show();
+			}
+			finish();
+		}
 	}
 }
